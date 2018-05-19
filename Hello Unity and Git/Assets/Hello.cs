8b14ff14fd87a1b2c1b68cc1a4b76a9ee806444b
Swatch <anotheredgydog@gmail.com>
2018-05-18 22:58:00 -0400

    Add Hello counter

--- Hello Unity and Git/Assets/Hello.cs
+++ Hello Unity and Git/Assets/Hello.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hello : MonoBehaviour {
+	public int nStart = 5;
+	private int n;
 
 	// Use this for initialization
 	void Start () {
-		
+		n = nStart;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		n += 1;
+		if (n > 600)
+			n = 0;
 	}
 }
