16390befd6172e234049bcb50b7856b9bf8f2791
Ozzy Callooh <ozzycallooh@gmail.com>
2018-05-18 22:50:14 -0400

    Updated Hello feature

--- Hello Unity and Git/Assets/Hello.cs
+++ Hello Unity and Git/Assets/Hello.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hello : MonoBehaviour {
+    public string message;
 
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log(message);
 	}
 	
 	// Update is called once per frame
